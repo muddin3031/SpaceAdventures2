a4ce9ace155273de1d56803e8f73595ec5dd8f64
BT_1N3_01 <jchen4465@bths.edu>
2018-12-11 08:30:28 -0500

    aq

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -37,7 +37,7 @@ public class Runner {
 
         while(gameOn)
         {
-            PlanetSwitch(space, ship1);
+            Room.PlanetSwitch(space, ship1);
             System.out.println("Where would you like to move? (Choose N, S, E, W)");
             String move = in.nextLine();
             if(validMove(move, ship1, space))
@@ -116,24 +116,5 @@ public class Runner {
         gameOn = false;
     }
 
-    public static void PlanetSwitch(Room[][] space, Ship ship1){
-        int r = ship1.getxPos();
-        int c = ship1.getyPos();
-        if(ship1.getxPos() == 0 && ship1.getyPos() > 0) {
-            space[r][c] = new Jupiter(r,c);
-        }
-        else if (ship1.getxPos() == 1) {
-            space[r][c] = new Venus(r,c);
-        }
-        else if (ship1.getxPos() == 2) {
-            space[r][c] = new Saturn(r,c);
-        }
-        else if(ship1.getxPos() == 3) {
-            space[r][c] = new Neptune(r,c);
-        }
-        else {
-            space[r][c] = new Uranus(r,c);
-        }
-    }
 
 }
--- src/Rooms/Room.java
+++ src/Rooms/Room.java
@@ -21,6 +21,26 @@ public class Room {
         apollo13 = null;
     }
 
+    public static void PlanetSwitch(Room[][] space, Ship ship1){
+        int r = ship1.getxPos();
+        int c = ship1.getyPos();
+        if(ship1.getxPos() == 0 && ship1.getyPos() > 0) {
+            space[r][c] = new Jupiter(r,c);
+        }
+        else if (ship1.getxPos() == 1) {
+            space[r][c] = new Venus(r,c);
+        }
+        else if (ship1.getxPos() == 2) {
+            space[r][c] = new Saturn(r,c);
+        }
+        else if(ship1.getxPos() == 3) {
+            space[r][c] = new Neptune(r,c);
+        }
+        else {
+            space[r][c] = new Uranus(r,c);
+        }
+    }
+
 
 
 }
